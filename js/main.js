b32d490db9ad119d53a7a1de282a0100c0fc8781
Graham Baker <graham.cr.baker@gmail.com>
2015-11-05 20:15:31 -0800

    getting there

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 $( document ).ready(function() {
 
-	$('#versing').click(function() {
-		var textSize = $('#versing').css('font-size');
+	init();
+	
+  $('.opts').hover(function() {
+    $(this).css('border-bottom', '2px solid #ff0000');
+    }, function() {
+      $(this).css('border-style', 'none');
+  });
 
-		// console.log(pxPeeler(textSize));
-		textSize = pxPeeler(textSize);
-		console.log(textSize);	
-		$('#versing').css('font-size', textSize);
-		$('#versing').append('<br>Versing');
+	$('#versing').click(function() {
+    $('#versing').append('<img class="logo" src="logo.png">');
+		var size = $('.logo').css('height');
+    console.log(size + ' size ');
+    var newSize = pxPeeler(size);
+    console.log(newSize + ' ->> ');
+    $('.logo').css('height', newSize);
+
 		}
 	);
 
@@ -19,7 +27,55 @@ $( document ).ready(function() {
 			i++;
 		}
 		console.log(toRet + ' in px peel');
-		toRet *= (parseFloat(".96"));
+		toRet *= (parseFloat(".92"));
 		return toRet + 'px';
 	}			
+  $('.ret').click(function () {
+    console.log(':)');
+    return false;
+  });
 });
+
+ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1I4ttgC67oMOIiL2iCBz4rhoaVJ2SagXClcoIFCiWvN0/pubhtml?gid=0&single=true';
+ var sheet_new_data;
+ var sheet_old_data;
+ function init() {
+    Tabletop.init( { key: public_spreadsheet_url,
+                     callback: showInfo } )
+ }
+
+  function showInfo(data, tabletop) {
+    $.each( tabletop.sheets(), function(i, sheet) {
+
+    	if(sheet.name === 'Old'){
+    		
+    		$('#old_show').append(tabler(sheet))
+    	}
+    	if(sheet.name === 'Shows'){
+    		
+    		$('#new_show').append(tabler(sheet)) 
+    	}
+    })
+  }
+
+  function tabler(sheet){
+  	// console.log(sheet.name)
+  	var sheetItems = sheet.all()
+  	
+  	var sheetContent = '<thead><tr><th>Date</th><th>Day</th><th>City</th><th>Location</th></tr></thead><tbody>';
+
+
+  	for (var i = 0; i < sheetItems.length; i++) {
+  		// console.log(sheetItems[i])
+  		sheetContent += '<tr>'
+  		sheetContent += '<td class="cell">' + sheetItems[i]['Date'] + '</td>'
+  		sheetContent += '<td class="cell">' + sheetItems[i]['Day'] + '</td>'
+  		sheetContent += '<td class="cell">' + sheetItems[i]['City'] + '</td>'
+  		sheetContent += '<td class="cell">' + '<a class="ret" href="' + sheetItems[i]['FB Event'] + '" > ' + sheetItems[i]['Location']+ '</a>' + '</td>'
+  		sheetContent += '</tr>'
+  	}
+
+  	sheetContent += '</tbody>'
+  	return sheetContent;
+  }
+
